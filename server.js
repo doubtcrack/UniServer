faa072ecbd1e8cd411d5350bd8841e2c9bef722b
doubtcrack <1.edujock@gmail.com>
2023-01-04 16:02:07 +0530

    update

--- server.js
+++ server.js
@@ -29,10 +29,10 @@ connection();
 
 //serve static file
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.use(express.static(path.join(__dirname, "build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
   // app.use(express.static(path.join(__dirname, "server_files")));
   // app.get("*", (req, res) => {
